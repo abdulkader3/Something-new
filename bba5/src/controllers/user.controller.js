9b5337f747f15385468a1569e5f5dbb3564bdc18
Abdul Kader <168454554+abdulkader3@users.noreply.github.com>
2025-04-09 20:19:43 +0600

    what ever

---
+++ bba5/src/controllers/user.controller.js
@@ -0,0 +1,78 @@
+import { User } from "../models/user.model.js";
+import { ApiErrors } from "../utils/ApiErrors.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandlers } from "../utils/asyncHandlers.js";
+import { UploadOnCloudinary } from "../utils/Cloudinary.js";
+
+
+
+const userRegister = asyncHandlers(async (req, res) => {
+  // get user data from front-end
+  const {userName,fullName,email,password} = req.body;
+
+
+  // Validation
+  if([userName,fullName,email,password].some((filed)=> filed.trim() === '')){
+    throw new ApiErrors(400, 'all filed are required')
+  }
+
+
+  // email Validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/ ;
+  if(!emailRegex.test(email)){
+    throw new ApiErrors(400, 'Invalid email format')
+  }
+
+
+  // check existed user
+  const existedUser = await User.findOne({
+    $or: [{email}, {userName}]
+  })
+
+  
+  // check for Image file
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  if(!avatarLocalPath){
+    throw new ApiErrors(400, 'avatar is required')
+  }
+
+
+  // upload Image files in cloud
+  const avatar = await UploadOnCloudinary(avatarLocalPath);
+  const coverImage = await UploadOnCloudinary(coverImageLocalPath);
+  if(!avatar){
+    throw new ApiErrors(400, 'avatar is required')
+  }
+
+
+  // create user Object - entry in db
+  const user = await User.create(
+    {
+      userName: userName.toLowerCase(),
+      fullName,
+      email,
+      password,
+      avatar: avatar.url,
+      coverImage: coverImage?.url || ''
+    }
+  )
+
+
+  // remove password and refreshToken from response
+  const userCreated = await User.findById(user._id).select(
+    "-password -refreshToken"
+  )
+
+  // check for user creation 
+  if(!userCreated){
+    throw new ApiErrors(500, 'something went wrong while registering user')
+  }
+
+
+  // return response
+  return res.status(201).json(
+    new ApiResponse(200, userCreated, 'user registered successfully')
+  )
+});
+export { userRegister };
---
+++ bba5/src/models/video.model.js
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const videoSchema = new mongoose.Schema({
+    videoFile: {
+        type: String,
+        required: true
+    }
+},{timestamps: true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
