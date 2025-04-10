49fb8b35e2cf5dc77206f1ec839bae5cb5d65529
Abdul Kader <168454554+abdulkader3@users.noreply.github.com>
2025-04-10 10:43:34 +0600

    8

---
+++ New folder/src/constance.js
@@ -0,0 +1,2 @@
+const DB_name = 'what'
+export{DB_name};
---
+++ New folder/src/utils/Cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_API_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+
+
+    //Upload on Cloud
+    const UploadOnCloudinary = async (LocalFilePath)=>{
+        try {
+            if(!LocalFilePath) return null;
+
+            const response = await cloudinary.uploader.upload(LocalFilePath, {
+                resource_type: "auto"
+            })
+
+            //success
+            console.log('upload success', response.url)
+
+            return response;
+
+        } catch (error) {
+            
+            fs.unlinkSync(LocalFilePath) // delete
+
+            return null;
+        }
+    }
+    export{UploadOnCloudinary}
