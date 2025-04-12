03b690badde469495e0a16cb90eb4c1055fcf0a6
Abdul Kader <168454554+abdulkader3@users.noreply.github.com>
2025-04-12 16:10:40 +0600

    what

---
+++ 19/src/utils/Cloudinary.js
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv';
+dotenv.config()
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+
+
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_API_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET 
+    });
+    
+   
+
+    const UploadOnCloudinary = async (localFilePath)=>{
+        try {
+
+            if(!localFilePath) return null;
+
+            //upload
+            const response = await cloudinary.uploader.upload(localFilePath,{
+                resource_type: 'auto'
+            })
+            
+            
+            //success
+            console.log("Upload success", response.url)
+
+            fs.unlinkSync(localFilePath);
+
+            return response;
+            
+        } catch (error) {
+
+            fs.unlinkSync(localFilePath) //delete
+
+            return null
+            
+        }
+    }
+    export{UploadOnCloudinary}
