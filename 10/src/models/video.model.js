4df4a48d3598b7161c8d157dd7ea2bf327e35266
Abdul Kader <168454554+abdulkader3@users.noreply.github.com>
2025-04-16 10:37:07 +0600

    Update video.model.js

--- 10/src/models/video.model.js
+++ 10/src/models/video.model.js
@@ -4,9 +4,18 @@ import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const videoSchema = new mongoose.Schema({
     videoFile: {
         type: String
-    }
+    }, category: {
+        type: String
+    }, duration: {
+        type: String
+    }, thumbnail: {
+        type: String
+    }, owner: {
+        type: String
+    },
 },{timestamps: true})
 
+videoSchema.plugin(mongooseAggregatePaginate);
 videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
