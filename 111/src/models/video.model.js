31c94f29fc11ae7d676bedf8b3ecb4dd6e44b2ae
Abdul Kader <168454554+abdulkader3@users.noreply.github.com>
2025-05-12 17:26:18 +0600

    Update video.model.js

--- 111/src/models/video.model.js
+++ 111/src/models/video.model.js
@@ -30,6 +30,34 @@ const videoSchema = new mongoose.Schema({
     },
     isPublished: {
         type: Boolean
+    },videoFile: {
+        type: String,
+        required: true
+    },
+    thumbnail: {
+        type: String,
+        required: [true, 'There is no fun without nay thumbnail']
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    title: {
+        type: String,
+        required: [true, "Its very important to give your video a title"]
+    },
+    description: {
+        type: String,
+    },
+    duration: {
+        type: Number,
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean
     }
 },{timestamps: true})
 
